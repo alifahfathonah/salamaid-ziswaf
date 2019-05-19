5fc7549f1dd3d9f3c1746200f51cb9ceaff97f50
ranitachi <fachran.nazarullah@gmail.com>
2019-05-19 23:03:10 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Model\Muzzaki;
 use App\Model\Petugas;
+use App\User;
 use App\Model\Transaksi_penerimaan;
 class HomeController extends Controller
 {
